5a13cce7353257e85cf260c4841a811652c9a695
Erikseeeeen <eivindseriksen@gmail.com>
2023-04-27 13:36:10 +0200

    yolov8 test

--- yolov8/draft.py
+++ yolov8/draft.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="coco128.yaml", epochs=3)  # train the model
+model.train(data="rdd2022.yaml", epochs=3)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
 results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 success = model.export(format="onnx")  # export the model to ONNX format
